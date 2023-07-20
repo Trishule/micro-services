a61b20c9ac22228ff57ceb11f007b9a6d1d43e5f
Trishul Gurumurthy <gurumruthytrishul078@gmail.com>
2023-07-20 15:49:13 +0530

    Added Files

---
+++ config-server/src/test/java/com/trishul/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trishul.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
